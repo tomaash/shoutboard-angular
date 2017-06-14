f0beae5992a0ac777d66d3681402b031be46e01a
Tomas Holas <tomas.holas@gmail.com>
2017-06-14 13:28:02 +0200

    computable props

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@angular/core'
+import { BehaviorSubject } from 'rxjs/BehaviorSubject'
 
 @Injectable()
 export class HomeService {
   message = 'Welcome to home page'
-  counter = 0
-  increment() {
-    this.counter++
+  counterSubject = new BehaviorSubject(0)
+  // Computed property can serve as basis for further computed properties
+  counterMessage = new BehaviorSubject('')
+  constructor() {
+    // Manually subscribe to each subject that couterMessage depends on
+    this.counterSubject.subscribe(this.recomputeCounterMessage)
   }
+
+  // Needs to have bound this
+  private recomputeCounterMessage = (x) => {
+    console.log('recompute counterMessage!')
+    this.counterMessage.next(`${x} ${x === 1 ? 'click' : 'clicks'} since last visit`)
+  }
+
+  increment() {
+    this.counterSubject.next(this.counterSubject.getValue() + 1)
+  }
+
 }
